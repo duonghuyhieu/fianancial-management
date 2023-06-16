1be1905d69266e56119cd5c26eb2565e23a4cd79
Duong Huy Hieu <gunnyhuyhieu@gmail.com>
2023-06-16 23:12:36 +0700

    hi

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Spend from './pages/Spend/layout/Spend';
-import Funding from './pages/Funding/layout/Funding';
-import Sidebar from './components/Sidebar/Sidebar';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Spend from "./pages/Spend/layout/Spend";
+import Funding from "./pages/Funding/layout/Funding";
+import Sidebar from "./components/Sidebar/Sidebar";
 function App() {
   return (
-    <div className='flex'>
-      <Sidebar />
+    <div className="flex">
       <Router>
+        <Sidebar />
         <Routes>
           <Route path="/" element={<Spend />} />
           <Route path="/funding" element={<Funding />} />
         </Routes>
       </Router>
     </div>
-
   );
 }
 
