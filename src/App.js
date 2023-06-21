2baf9c8dcf58ab47f0729d03df006d6a018b1c06
unknown <minhcva98@gmail.com>
2023-06-21 18:14:53 +0700

    hi

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 import Spend from "./pages/Spend/layout/Spend";
 import Funding from "./pages/Funding/layout/Funding";
 import Sidebar from "./components/Sidebar/Sidebar";
 function App() {
   return (
     <div className='flex'>
-      <Router>
         <Sidebar />
         <Routes>
           <Route path="/" element={<Spend />} />
           <Route path="/funding" element={<Funding />} />
         </Routes>
-      </Router>
     </div>
   );
 }
--- src/pages/Spend/components/AddSpend.js
+++ src/pages/Spend/components/AddSpend.js
@@ -7,38 +7,68 @@ Modal.setAppElement("#root");
 
 function AddSpend() {
   const [isOpen, setIsOpen] = useState(false);
-
-  const [type, setType] = useState("chi");
+  const [type, setType] = useState("payment");
 
   const openModal = () => {
     setIsOpen(true);
   };
 
-  const closeModal = () => {
+  const closeModal = (resetForm) => {
     setIsOpen(false);
+    resetForm();
   };
 
-  const handleSubmit = (values) => {
+  const handleSubmit = (values, { setSubmitting, resetForm }) => {
     values.type = type;
-      console.log(values);
-      closeModal()
-    };
+    console.log(values);
+    closeModal(resetForm);
+    setSubmitting(false);
+  };
 
   const initialValues = {
-    type: "",
+    type: "payment",
     date: "",
-    account: "",
-    typeMoney: "",
+    sourceFund: "bank",
+    typeMoney: "eating",
     money: "",
   };
 
-  const validateForm = (values) => {};
+  const validateForm = (values) => {
+    // Add your form validation logic here if needed
+  };
 
   const handleType = (text) => {
     setType(text);
   };
 
-  const items = ["chi", "thu"];
+  const types = [
+    { name: "Chi", value: "payment" },
+    { name: "Thu", value: "income" },
+  ];
+  const sourceFunds = [
+    { name: "Thẻ ngân hàng", value: "bank" },
+    { name: "Tiền mặt", value: "money" },
+    { name: "Thẻ tín dụng", value: "credit" },
+  ];
+  const paymentTypes = [
+    { name: "Ăn uống", value: "eating" },
+    { name: "Giải trí", value: "entertainment" },
+    { name: "Giao thông", value: "traffic" },
+    { name: "Sở thích", value: "hobby" },
+    { name: "Sinh hoạt", value: "living" },
+    { name: "Quần áo", value: "clothes" },
+    { name: "Sức khỏe", value: "health" },
+    { name: "Giáo dục", value: "education" },
+    { name: "Sự kiện", value: "event" },
+    { name: "Khác", value: "other" },
+  ];
+  const incomeTypes = [
+    { name: "Tiền lương", value: "salary" },
+    { name: "Thu nhập từ đầu tư", value: "investments" },
+    { name: "Thu nhập từ cho thuê", value: "rental" },
+    { name: "Thu nhập từ kinh doanh", value: "business" },
+    { name: "Khác", value: "other" },
+  ];
 
   return (
     <div>
@@ -59,7 +89,7 @@ function AddSpend() {
         }}
       >
         <CloseIcon
-          onClick={closeModal}
+          onClick={() => closeModal}
           className="cursor-pointer absolute right-4"
         />
         <h2 className="text-2xl py-6 px-2">Quản lý tài chính</h2>
@@ -67,55 +97,101 @@ function AddSpend() {
           initialValues={initialValues}
           validate={validateForm}
           onSubmit={handleSubmit}
+          enableReinitialize={true}
         >
-          <Form>
-            <div className="flex cursor-pointer justify-around">
-              {items.map((item, index) => (
-                <div
-                  key={index}
-                  className={`px-[20%] rounded-lg border-2 border-black ${
-                    type === item ? "bg-slate-200" : ""
-                  }`}
-                  onClick={() => {
-                    handleType(item);
-                  }}
-                >
-                  {item}
-                </div>
-              ))}
-            </div>
+          {({ errors, touched, resetForm }) => (
+            <Form>
+              <div className="flex cursor-pointer justify-around">
+                {types.map((item, index) => (
+                  <div
+                    key={index}
+                    className={`px-[20%] rounded-lg border-2 border-black ${
+                      type === item.value ? "bg-slate-200" : ""
+                    }`}
+                    onClick={() => {
+                      handleType(item.value);
+                    }}
+                  >
+                    {item.name}
+                  </div>
+                ))}
+              </div>
 
-            <div className="pt-8 w-full">
-              <label htmlFor="date">Ngày</label>
-                <Field     className="w-full mt-2 py-1 px-2 border border-gray-600 rounded transition duration-300 focus:border-blue-500"
-                              type="date" id="date" name="date" />
+              <div className="pt-8 w-full">
+                <label htmlFor="date">Ngày</label>
+                <Field
+                  className="w-full mt-2 py-1 px-2 border border-gray-600 rounded transition duration-300 focus:border-blue-500"
+                  type="date"
+                  id="date"
+                  name="date"
+                />
                 <ErrorMessage name="date" component="div" />
-            </div>
+              </div>
 
-            <div className="pt-8 w-full">
-              <label htmlFor="account">Nguồn tiền</label>
-                <Field className="w-full mt-2 py-1 px-2 border border-gray-600 rounded transition duration-300 focus:border-blue-500"
-                        type="text" id="account" name="account" />
-              <ErrorMessage name="account" component="div" />
-            </div>
+              <div className="pt-8 w-full">
+                <label htmlFor="sourceFund">Nguồn tiền</label>
+                <Field
+                  as="select"
+                  name="sourceFund"
+                  className="w-full mt-2 py-1 px-2 border border-gray-600 rounded transition duration-300 focus:border-blue-500"
+                >
+                  {sourceFunds.map((item, index) => (
+                    <option key={index} value={item.value}>
+                      {item.name}
+                    </option>
+                  ))}
+                </Field>
+                <ErrorMessage name="sourceFund" component="div" />
+              </div>
 
-            <div className="pt-8 w-full">
-              <label htmlFor="typeMoney">Thể loại</label>
-                <Field className="w-full mt-2 py-1 px-2 border border-gray-600 rounded transition duration-300 focus:border-blue-500"
-                    type="text" id="typeMoney" name="typeMoney" />
-              <ErrorMessage name="typeMoney" component="div" />
-            </div>
+              <div className="pt-8 w-full">
+                <label htmlFor="typeMoney">Thể loại</label>
+                <Field
+                  as="select"
+                  name="typeMoney"
+                  className="w-full mt-2 py-1 px-2 border border-gray-600 rounded transition duration-300 focus:border-blue-500"
+                >
+                  {type === "payment"
+                    ? paymentTypes.map((item, index) => (
+                        <option key={index} value={item.value}>
+                          {item.name}
+                        </option>
+                      ))
+                    : incomeTypes.map((item, index) => (
+                        <option key={index} value={item.value}>
+                          {item.name}
+                        </option>
+                      ))}
+                </Field>
+                <ErrorMessage name="typeMoney" component="div" />
+              </div>
 
-            <div className="pt-8 w-full">
-              <label htmlFor="money">Số tiền</label>
-              <Field  className="w-full mt-2 py-1 px-2 border border-gray-600 rounded transition duration-300 focus:border-blue-500"
-               type="number" id="money" name="money" />
-              <ErrorMessage name="money" component="div" />
-            </div>
-
-            <button onClick={closeModal}>Đóng dialog</button>
-            <button type="submit">Xác nhận</button>
-          </Form>
+              <div className="pt-8 w-full pb-14">
+                <label htmlFor="money">Số tiền</label>
+                <Field
+                  className="w-full mt-2 py-1 px-2 border border-gray-600 rounded transition duration-300 focus:border-blue-500"
+                  type="number"
+                  id="money"
+                  name="money"
+                />
+                <ErrorMessage name="money" component="div" />
+              </div>
+              <div className="absolute bottom-4 right-4">
+                <button
+                  onClick={() => closeModal(resetForm)}
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mr-2 rounded"
+                >
+                  Đóng dialog
+                </button>
+                <button
+                  type="submit"
+                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Xác nhận
+                </button>
+              </div>
+            </Form>
+          )}
         </Formik>
       </Modal>
     </div>
